45ca9c1bc405d111cfb9deede6b82fb25c6027a1
天渊 <117790537+tianyuanlizian@users.noreply.github.com>
2023-11-29 20:11:36 +0800

    Initial commit

---
+++ src/main/java/edu/nf/generalbook/GeneralBookApplication.java
@@ -0,0 +1,13 @@
+package edu.nf.generalbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneralBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeneralBookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/nf/generalbook/GeneralBookApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.nf.generalbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneralBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
