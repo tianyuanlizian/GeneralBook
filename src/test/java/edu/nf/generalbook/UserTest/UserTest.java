a13a1985c1f2d8152c09e712309e554f9f7731e9
天渊 <117790537+tianyuanlizian@users.noreply.github.com>
2023-11-30 15:16:11 +0800

    更新 UserTest.java
    测试UserDao

--- src/test/java/edu/nf/generalbook/UserTest/UserTest.java
+++ src/test/java/edu/nf/generalbook/UserTest/UserTest.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
 /**
  * @Author：tianyuan
  * @Date：2023/11/30 11:46
@@ -21,14 +23,46 @@ public class UserTest {
     @Test
     void addTest(){
         User user = new User();
+        user.setName("测试");
+        user.setAccount("12123124");
+        user.setPassword("123456");
+        user.setSex("男");
+        user.setEmail("4690590312@qq.com");
+        user.setPhoto("34444444545");
+        user.setPhone("564736743712");
+        user.setState("1");
+        dao.addUser(user);
+    }
+
+    @Test
+    void updTest(){
+        User user = new User();
+        user.setUId(1);
         user.setName("骚别");
         user.setAccount("12123124");
         user.setPassword("qin123");
         user.setSex("男");
         user.setEmail("2562346723@qq.com");
         user.setPhoto("3r8265rduiwh");
-        user.setPhone("131423545");
+        user.setPhone("18870148284");
         user.setState("1");
-        dao.addUser(user);
+        dao.updUser(user);
+    }
+    @Test
+    void updState(){
+        dao.updUserState(1,"0");
+    }
+
+    @Test
+    void listUser(){
+        List<User> list = dao.listUser();
+        list.forEach(user -> {
+            log.info(user.toString());
+        });
+    }
+    @Test
+    void countTest(){
+        Long count = dao.count();
+        log.info(count.toString());
     }
 }
